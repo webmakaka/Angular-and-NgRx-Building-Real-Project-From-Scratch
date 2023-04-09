4f65844177b34243e2b0dc5c320f9599f89107a2
Marley <marley.nodejs@gmail.com>
2023-04-09 03:06:03 +0300

    refactor

--- app/src/app/shared/types/currentUserInput.interface.ts
+++ app/src/app/shared/types/currentUserInput.interface.ts
@@ -1,14 +1,19 @@
-// не знаю как сделать extends CurrentUserInterface
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 export interface CurrentUserInputInterface {
-  user: {
-    id: number;
-    username: string;
-    email: string;
-    bio?: string | null;
-    image: string | null;
-    token: string;
-    createdAt: string;
-    updatedAt: string;
-    password: string;
-  };
+  user: CurrentUserInterface & { password: string };
 }
+
+// Если не будет работать
+// export interface CurrentUserInputInterface {
+//   user: {
+//     id: number;
+//     username: string;
+//     email: string;
+//     bio?: string | null;
+//     image: string | null;
+//     token: string;
+//     createdAt: string;
+//     updatedAt: string;
+//     password: string;
+//   };
+// }
